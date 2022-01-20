656e0ba2a9a1bc2a5a3c44eade9f1fe36a07df3b
chengyi ren <983560794@qq.com>
2022-01-20 20:48:00 +0800

    打包测试

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,16 @@ module.exports = {
         'views': '@/views',
       }
     }
-  }
+  },
+  
+  assetsDir:'static',
+  parallel:false,
+  publicPath:'./',
+  devServer:{
+      port:5000,//端口号
+      open:true, //是否自启动
+  
+}
+
 }
 
