d6b967a79c5f404cd06b76087b1cfac7729b2fbe
chengyi ren <983560794@qq.com>
2022-01-20 16:14:20 +0800

    新增Home调取多个数据内容

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,21 +3,27 @@
     <nav-bar class="home_navbar">
       <template v-slot:center>购物车</template>
     </nav-bar>
-
   </div>
 </template>
 
 <script >
-
+import { getHomeMultidata } from "network/home";
 import NavBar from "components/common/navbar/NavBar";
 export default {
   data() {
     return {
-
+      banner: null,
+      recommend: null,
     };
   },
   components: {
     NavBar,
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      (this.banner = res.data.banner);
+      this.recommend = res.data.recommend
+    });
   },
 };
 </script>
