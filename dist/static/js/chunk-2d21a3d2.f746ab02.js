77c9fed2669594d89ae2c0c10ba0cba62e5bdd49
chengyi ren <983560794@qq.com>
2022-01-22 19:22:10 +0800

    打包20220122

---
+++ dist/static/js/chunk-2d21a3d2.f746ab02.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{},[t("h2",[e._v("分类")])])}],c={name:"",data(){return{}},components:{},methods:{}},u=c,s=t("2877"),l=Object(s["a"])(u,r,a,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d21a3d2.f746ab02.js.map
